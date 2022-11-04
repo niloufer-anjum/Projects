e3c4ea975b996d5e23aaf702dc6d4d4621d993e4
Niloufer <nilu175@gmail.com>
2022-11-04 22:41:09 +0530

    Madlibs

---
+++ Python/YouTube/madlibs.py
@@ -0,0 +1,17 @@
+name = input("Enter a name: ")
+age = input("Enter age: ")
+place = input("Enter a place: ")
+animal = input("Enter an animal: ")
+thing = input("Enter a thing: ")
+emotion1 = input("Enter an emotion: ")
+emotion2 = input("Enter another emotion: ")
+emotion3 = input("Enter another emotion: ")
+activity = input("Enter an activity: ")
+color = input("Enter a color: ")
+
+madlibs = f"{name} was a healthy, wealthy and wise person. Aged {age}, \
+{name} lived in {place} and hated {animal}. Loved {thing}s and was \
+usually {emotion1}, {emotion2} and {emotion3}. Today {name} was {activity} \
+with a {color} ribbon!"
+
+print(madlibs)
